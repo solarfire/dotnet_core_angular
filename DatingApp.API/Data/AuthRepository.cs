fab26ab2cd991e3dcdd0b6f0224333b5a936fefd
craig <solksjaer64@hotmail.com>
2020-04-08 22:12:56 +0200

    bug fix with login

--- DatingApp.API/Data/AuthRepository.cs
+++ DatingApp.API/Data/AuthRepository.cs
@@ -76,7 +76,7 @@ namespace DatingApp.API.Data
                 var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
 
                 for (int i=0; i <computedHash.Length; i ++) {
-                    if (computedHash[i] == passwordHash[i])
+                    if (computedHash[i] != passwordHash[i])
                         return false;
                 }
             }
